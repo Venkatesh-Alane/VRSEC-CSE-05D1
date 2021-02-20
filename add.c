3ab4990834715dbe06e656cad5949a4deb3ad01a
venkatesh <venkateshalane2001@gmail.com>
2021-02-20 10:24:44 +0530

    barrey pandajith

--- add.c
+++ add.c
@@ -2,11 +2,12 @@
 #include<stdio.h>
 void main()
 {
-int a,b;
+int a,b,c;
 printf("Arey pulka");
 printf("enter a,b values");
 scanf("%d%d",&a,&b);
-printf("Add:%d",a+b);
+c=a+b;
+printf("Add:%d",c);
 getch();
 return o;
 }
